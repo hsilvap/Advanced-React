d19355ae0e0e8c30511bed36ede06a5dc71e2b13
hs <hsilvap@me.com>
2019-01-30 17:18:24 -0600

    Module 3 finished

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,19 @@
-const mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info){
+        //TODO check login
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        },info);
+        console.log(item);
+        return item;
+    }
+    //createDog(parent, args, ctx, info){
+        //create a dog!
+    //    console.log(args)
+    //}
 
-module.exports = mutations;
+};
+
+module.exports = Mutations;
